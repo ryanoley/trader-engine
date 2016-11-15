2568eca6ca4780e1229894d2444782ab337bf4d4
Chad <chadpatel@gmail.com>
2016-11-14 21:01:42 -0500

    updated python code : ZeroMQ for Roundabout
    Task-Url:

--- Roundabout/src/python/tradeClient.py
+++ Roundabout/src/python/tradeClient.py
@@ -1,8 +1,6 @@
 # Task ventilator
 # Binds PUSH socket to tcp://localhost:5557
 # Sends batch of tasks to workers via that socket
-#
-# Author: Lev Givon <lev(at)columbia(dot)edu>
 
 import zmq
 import random
@@ -20,27 +18,29 @@ context = zmq.Context()
 sender = context.socket(zmq.PUSH)
 sender.connect("tcp://192.168.0.101:5557")
 
-print("Press Enter when the workers are ready: ")
-_ = raw_input()
-print("Sending tasks to workers")
+print("Enter order to send : ")
+order = input()
+print("Sending : %s" %order)
 
 # The first message is "0" and signals start of batch
 
 # Initialize random number generator
-random.seed()
+# random.seed()
 
 # Send 100 tasks
-total_msec = 0
-for task_nbr in range(100):
+# total_msec = 0
+while (order != "done"):
+#for task_nbr in range(100):
 
     # Random workload from 1 to 100 msecs
-    workload = random.randint(1, 100)
-    total_msec += workload
+ #   workload = random.randint(1, 100)
+ #   total_msec += workload
 
-    sender.send_string(u'%i' % workload)
-    time.sleep(1)
+    sender.send_string(order)
+    order = raw_input()
+#    time.sleep(1)
 
-print("Total expected cost: %s msec" % total_msec)
+print("Quitting")
 
 # Give 0MQ time to deliver
 time.sleep(1)

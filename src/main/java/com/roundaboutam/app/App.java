39ab240d1b011296e127707240ade2f6a6d2b743
mitchellsuter <mitchellasuter@gmail.com>
2016-12-29 08:04:02 -0500

    MOD: main entry now has some basic functionality

--- src/main/java/com/roundaboutam/app/App.java
+++ src/main/java/com/roundaboutam/app/App.java
@@ -1,13 +1,20 @@
 package com.roundaboutam.app;
 
-/**
- * Hello world!
- *
- */
+import com.roundaboutam.app.orders.FixOrderFormatter;
+import com.roundaboutam.app.orders.Order;
+import com.roundaboutam.app.orders.OrderSide;
+import com.roundaboutam.app.orders.OrderType;
+
+import quickfix.fix42.NewOrderSingle;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Order vo = new Order(123, "AAPL", OrderType.VWAP, OrderSide.BUY, 100);
+    	System.out.println("Order created for: " + vo.getInstrumentId());
+    	NewOrderSingle no = FixOrderFormatter.getNewOrder(vo);
+    	System.out.println(no.toString());
     }
 }

9b7afd77c131e07da6d4ecd650979d4c657d52d5
Mitchell Suter <mitchellasuter@gmail.com>
2017-01-11 17:34:57 -0500

    BUG: with shutdown needed to ensure all log offs

--- src/main/java/com/roundaboutam/trader/TraderEngine.java
+++ src/main/java/com/roundaboutam/trader/TraderEngine.java
@@ -1,5 +1,6 @@
 package com.roundaboutam.trader;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.CountDownLatch;
 
@@ -10,17 +11,16 @@ import org.slf4j.LoggerFactory;
 
 import com.roundaboutam.trader.ui.TraderFrame;
 
+import quickfix.ConfigError;
 import quickfix.DefaultMessageFactory;
+import quickfix.FileLogFactory;
 import quickfix.FileStoreFactory;
 import quickfix.Initiator;
-import quickfix.LogFactory;
-import quickfix.MessageFactory;
-import quickfix.MessageStoreFactory;
-import quickfix.ScreenLogFactory;
 import quickfix.Session;
 import quickfix.SessionID;
 import quickfix.SessionSettings;
 import quickfix.SocketInitiator;
+
 import org.quickfixj.jmx.JmxExporter;
 
 public class TraderEngine {
@@ -28,37 +28,42 @@ public class TraderEngine {
 	private static final CountDownLatch shutdownLatch = new CountDownLatch(1);
 
 	private static final Logger log = LoggerFactory.getLogger(TraderEngine.class);
+	private static boolean initiatorStarted = false;
+
 	private static TraderEngine traderEngine;
-	private boolean initiatorStarted = false;
-	private Initiator initiator = null;
+	private static Initiator initiator;
+	private TraderApplication application;
 
 	public TraderEngine() throws Exception {
 
+		SessionSettings settings = getSettings();
+
+		application = new TraderApplication(settings);
+
+        initiator = new SocketInitiator(
+        		application, 
+        		new FileStoreFactory(settings), 
+        		settings, 
+        		new FileLogFactory(settings),
+        		new DefaultMessageFactory());
+
+        JmxExporter exporter = new JmxExporter();
+        exporter.register(initiator);
+
+        new TraderFrame(application);
+	}
+
+	private SessionSettings getSettings() throws ConfigError, IOException {
 		InputStream inputStream = TraderEngine.class.getResourceAsStream("FIXConfig.cfg");
         SessionSettings settings = new SessionSettings(inputStream);
         inputStream.close();
-
-        boolean logHeartbeats = Boolean.valueOf(System.getProperty("logHeartbeats", "false"));
 
         // Workaround for env variables
         String rootpath = System.getenv("DATA") + "\\trader-engine\\LOGS";
         settings.setString("FileStorePath", rootpath + "\\fileStore");
         settings.setString("FileLogPath", rootpath + "\\logs");
         settings.setString("CustomLogPath", rootpath + "\\custom");
-
-        TraderApplication application = new TraderApplication(settings);
-
-        MessageStoreFactory messageStoreFactory = new FileStoreFactory(settings);
-        LogFactory logFactory = new ScreenLogFactory(true, true, true, logHeartbeats);
-        MessageFactory messageFactory = new DefaultMessageFactory();
-
-        initiator = new SocketInitiator(application, messageStoreFactory, settings, logFactory,
-                messageFactory);
-
-        JmxExporter exporter = new JmxExporter();
-        exporter.register(initiator);
-
-        new TraderFrame(application);
+        return settings;
 	}
 
     public synchronized void logon() {
@@ -71,39 +76,40 @@ public class TraderEngine {
                 log.error("Logon failed", e);
             }
         } else {
-            for (SessionID sessionId : initiator.getSessions()) {
-            	Session.lookupSession(sessionId).logon();
-            }
+	        for (SessionID sessionId : initiator.getSessions()) {
+	        	Session.lookupSession(sessionId).logon();
+	        }
         }
     }
 
     public void logout() {
-    	System.out.println("TraderEngine.logout() invoked");
     	for (SessionID sessionId : initiator.getSessions()) {
-        	System.out.println("Shutting down session: " + sessionId.toString());
-            Session.lookupSession(sessionId).logout("User requested");
+    		if (Session.lookupSession(sessionId).isLoggedOn())
+    			Session.lookupSession(sessionId).logout("User requested");
         }
     }
 
     public void stopInitiator() {
-    	System.out.println("TraderEngine.stopInitiator() invoked");
     	logout();
     	if(initiatorStarted)
-        	initiator.stop();
+    		initiator.stop();
         initiatorStarted = false;
     }
-    
+
     public boolean getInitiatorState() {
     	return initiatorStarted;
     }
 
     public void shutdown() {
     	System.out.println("TraderEngine.shutdown() invoked");
-    	try {
-    		logout();
-    		shutdownLatch.countDown();
-    	} catch (Exception e) { }
-    	System.exit(0);
+    	System.out.println(application.getSessionIDs().size());
+    	logout();
+        initiator.stop();
+        while (application.getSessionIDs().size() > 0) {
+        	try { Thread.sleep(1000); } catch (InterruptedException e) { }
+        }
+        shutdownLatch.countDown();
+        System.exit(0);
     }
 
 	public static TraderEngine get() {
@@ -117,7 +123,6 @@ public class TraderEngine {
             log.info(e.getMessage(), e);
         }
     	traderEngine = new TraderEngine();
-
         shutdownLatch.await();
     }
 

823a57854181d94fce7c97e882c396db0f591282
ryanoley <ryan@roundaboutam.com>
2017-01-30 12:02:44 -0500

    MOD: Correct test

--- src/test/java/com/roundaboutam/trader/execution/ExecutionTest.java
+++ src/test/java/com/roundaboutam/trader/execution/ExecutionTest.java
@@ -11,7 +11,7 @@ public class ExecutionTest extends TestCase {
     			"1234", "1234", "BRK", "20160101-14:45:64", 
     			OrderSide.BUY, 100, 100.5);
 		execution.setSuffix("B");
-        assertEquals(execution.getLogEntry(), "1,1234,1234,BRK/B,20160101-14:45:64,NYSE,Buy,100,100.5,0.0,0.0,null");
+        assertEquals(execution.getLogEntry(), "1,1234,1234,BRK/B,20160101-14:45:64,Buy,100,100.5,0.0,0.0,null");
 	}
 
 }
